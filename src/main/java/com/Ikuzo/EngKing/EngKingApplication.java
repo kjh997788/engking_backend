160a9f208097c85e83f98f6c6d1f8037c85ae134
JAEHYUNGKWAK <kjh997788@gmail.com>
2024-08-23 16:56:00 +0900

    init

---
+++ src/main/java/com/Ikuzo/EngKing/EngKingApplication.java
@@ -0,0 +1,16 @@
+package com.Ikuzo.EngKing;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "My API", version = "v1", description = "My API Description"))
+public class EngKingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EngKingApplication.class, args);
+	}
+
+}
