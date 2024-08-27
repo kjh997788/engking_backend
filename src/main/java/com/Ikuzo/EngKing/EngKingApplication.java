593e7b09bcf12a98bcb2f74f2b3c114482b660ed
JAEHYUNGKWAK <kjh997788@gmail.com>
2024-08-27 14:06:07 +0900

    채팅방 관련

--- src/main/java/com/Ikuzo/EngKing/EngKingApplication.java
+++ src/main/java/com/Ikuzo/EngKing/EngKingApplication.java
@@ -4,6 +4,8 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "My API", version = "v1", description = "My API Description"))
@@ -13,4 +15,8 @@ public class EngKingApplication {
 		SpringApplication.run(EngKingApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
--- src/main/java/com/Ikuzo/EngKing/service/ChatRoomService.java
+++ src/main/java/com/Ikuzo/EngKing/service/ChatRoomService.java
@@ -3,8 +3,10 @@ package com.Ikuzo.EngKing.service;
 import com.Ikuzo.EngKing.entity.ChatRoom;
 import com.Ikuzo.EngKing.entity.ChatMessage;
 import com.Ikuzo.EngKing.repository.ChatRoomRepository;
+import com.Ikuzo.EngKing.dto.LangchainMessageRequestDto;  // 수정된 DTO 클래스 임포트
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -15,6 +17,9 @@ public class ChatRoomService {
 
     @Autowired
     private ChatRoomRepository chatRoomRepository;
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     public ChatRoom createChatRoom(String memberId, String topic, String difficulty) {
         ChatRoom chatRoom = new ChatRoom();
@@ -32,8 +37,18 @@ public class ChatRoomService {
 
         if (optionalChatRoom.isPresent()) {
             ChatRoom chatRoom = optionalChatRoom.get();
+
+            // 외부 서버로 POST 요청 보내기
+            String url = "http://external-server-url/api/processMessage";
+            LangchainMessageRequestDto langchainMessageRequestDto = LangchainMessageRequestDto.from(chatRoomId, message.getContent());
+            String responseMessage = restTemplate.postForObject(url, langchainMessageRequestDto, String.class);
+
+            // 응답받은 메시지로 기존 메시지 내용 대체
+            message.setContent(responseMessage);
+
             chatRoom.getMessages().add(message);
             chatRoomRepository.save(chatRoom);
+
             return message;
         } else {
             throw new RuntimeException("Chat room not found with id: " + chatRoomId);
